6b46357e2a5b5f51ba37938eb1018e1ab646202b
Christen Weston <paleeserecycle@gmail.com>
2021-12-05 16:49:24 -0800

    Added in reset roundScore function

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 function Player(playerNum) {
   this.playerNumber = playerNum;
   this.score = 0;
+  this.totalScore = 0;
 }
 
 function Rolldice(){
@@ -33,15 +34,29 @@ function turnOver(diceRoll, player) {
   return 0;
 }
 
-function calculateScore(diceRoll, player) {
+function calculateRoundScore(diceRoll, player) {
     if (diceRoll === 1){
+       player.score = 0;
     }
     else if (diceRoll !== 1){ 
-      player.score +=diceRoll; 
+      player.score += diceRoll; 
     }
     return player.score;
   }
-  
+
+function calculateTotalScore(roundScore, player) {
+  let totalScore = (player.totalScore + roundScore);
+  player.totalScore += roundScore;
+  console.log(player.totalScore);
+  return totalScore;
+}
+
+function resetRoundScore(roundScore, player) {
+  player.score = 0;
+  console.log("Player.score" + player.score + "Player total score " + player.totalScore);
+  return 0;
+}
+
 function playerWon(score, player) {
   let playerScore = score;
   let whoWon = "";
@@ -49,13 +64,12 @@ function playerWon(score, player) {
     whoWon = "Player " + player.playerNumber + " won! Game over";
     return whoWon;
   }
-  //else if (playerScore <100) {
- // }
 }
 
 //User Interface
 let player1;
 let player2;
+//let player3;
 
 function disableTurn(turnOver) {
   if (turnOver === "Player 2's turn") {
@@ -83,6 +97,9 @@ $(document).ready(function() {
   $("#game").show();
   player1 = new Player(1);
   player2 = new Player(2);
+  diceLog1 = [];
+  diceLog2 = [];
+  //player3 = new Player(3);
   });
 });
 
@@ -90,12 +107,13 @@ $(document).ready(function() {
   $(".btn-roll1").click(function(event){
   event.preventDefault();
   dice = Rolldice();
+  diceLog1.push(dice);
   var turnOver1 = turnOver(dice, player1); 
-  score = calculateScore(dice, player1);
-  winner = playerWon(score, player1);
+  roundScore = calculateRoundScore(dice, player1);
+  console.log("Round score " + roundScore + " dice " + dice);
+
   $(".playerTurn").text(turnOver1);
-  $(".winner").text(winner);
-  $(".player1-score").text(score);
+  $(".player1-score").text(player1.totalScore);
   $(".player1-roll").text(dice);
   });
 });
@@ -104,12 +122,15 @@ $(document).ready(function() {
   $(".btn-roll2").click(function(event){
   event.preventDefault();
   dice = Rolldice();
+  diceLog2.push(dice);
   var turnOver2 = turnOver(dice, player2);
-  score = calculateScore(dice, player2);
-  winner = playerWon(score, player2);
+  roundScore = calculateRoundScore(dice, player2);
+  console.log("Round score " + roundScore + " dice " + dice);
+ // score = calculateTotalScore(roundScore, player2);
+  
  $(".playerTurn").text(turnOver2);
- $(".winner").text(winner);
- $(".player2-score").text(score);
+ 
+ $(".player2-score").text(player2.totalScore);
  $(".player2-roll").text(dice);
   });
 });
@@ -117,6 +138,12 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".btn-hold1").click(function(event){
   event.preventDefault();
+  score = calculateTotalScore(roundScore, player1);
+  roundScore = resetRoundScore(roundScore, player1);
+  console.log("Reset round score " + roundScore + " roundScore for player 1 should be 0: " + player1.score);
+  winner = playerWon(score, player1);
+  $(".winner").text(winner);
+  $(".player1-score").text(score);
   var player1Hold = "Player 1 holds. Player 2's turn";
   $(".playerTurn").text(player1Hold);
   disableTurn(player1Hold);
@@ -126,6 +153,12 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".btn-hold2").click(function(event){
   event.preventDefault();
+  score = calculateTotalScore(roundScore, player2);
+  roundScore = resetRoundScore(roundScore, player2);
+  console.log("Reset round score " + roundScore + " roundScore for player 2 should be 0: " + player2.score);
+  winner = playerWon(score, player2);
+  $(".winner").text(winner);
+  $(".player2-score").text(score);
   var player2Hold = "Player 2 holds. Player 1's turn";
   $(".playerTurn").text(player2Hold);
   disableTurn(player2Hold);
