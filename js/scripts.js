be51db6547d78ef3c4aa7558a5a67a6be7ca56e1
Christen Weston <paleeserecycle@gmail.com>
2021-12-06 19:36:51 -0800

    Added in reset round score

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,22 @@ function Player(playerNum) {
   this.totalScore = 0;
 }
 
-function Rolldice(){
+Player.prototype.Rolldice = function(){
   let diceRoll = Math.floor((Math.random() * 6) + 1);
   return diceRoll;
 }
 //Could call playerWon from here and factor that in to return
-function turnOver(diceRoll, player) {
+Player.prototype.turnOver = function(diceRoll, player) {
   let whosTurn = "";
   playerId = player.playerNumber;
   if (diceRoll === 1 && playerId === 1) {
     let whosTurn = "Player 2's turn";
-    disableTurn(whosTurn);
+    Player.prototype.disableTurn(whosTurn);
     return whosTurn;
     }
   else if (diceRoll === 1 && playerId === 2) {
     let whosTurn = "Player 1's turn";
-    disableTurn(whosTurn);
+    Player.prototype.disableTurn(whosTurn);
     return whosTurn;
     }
   else if (diceRoll !== 1 && playerId === 1) {
@@ -34,7 +34,7 @@ function turnOver(diceRoll, player) {
   return 0;
 }
 
-function calculateRoundScore(diceRoll, player) {
+Player.prototype.calculateRoundScore = function(diceRoll, player) {
     if (diceRoll === 1){
        player.score = 0;
     }
@@ -44,20 +44,22 @@ function calculateRoundScore(diceRoll, player) {
     return player.score;
   }
 
-function calculateTotalScore(roundScore, player) {
-  let totalScore = (player.totalScore + roundScore);
-  player.totalScore += roundScore;
+Player.prototype.calculateTotalScore = function(player) {
+  let totalScore = (player.totalScore) + player.score;
+  //let totalScore = (player.totalScore + roundScore);
+  player.totalScore += player.score;
+  //player.totalScore += roundScore;
   console.log(player.totalScore);
   return totalScore;
 }
 
-function resetRoundScore(roundScore, player) {
+Player.prototype.resetRoundScore = function(roundScore, player) {
   player.score = 0;
   console.log("Player.score" + player.score + "Player total score " + player.totalScore);
   return 0;
 }
 
-function playerWon(score, player) {
+Player.prototype.playerWon = function(score, player) {
   let playerScore = score;
   let whoWon = "";
   if (playerScore >= 100) {
@@ -71,7 +73,7 @@ let player1;
 let player2;
 //let player3;
 
-function disableTurn(turnOver) {
+Player.prototype.disableTurn = function(turnOver) {
   if (turnOver === "Player 2's turn") {
     $("#player2").toggle();
     $("#player1").toggle();
@@ -106,12 +108,12 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".btn-roll1").click(function(event){
   event.preventDefault();
-  dice = Rolldice();
+  dice = Player.prototype.Rolldice();
   diceLog1.push(dice);
-  var turnOver1 = turnOver(dice, player1); 
-  roundScore = calculateRoundScore(dice, player1);
+  var turnOver1 = Player.prototype.turnOver(dice, player1); 
+  roundScore = Player.prototype.calculateRoundScore(dice, player1);
   console.log("Round score " + roundScore + " dice " + dice);
-
+  $(".player1-roundScore").text(roundScore);
   $(".playerTurn").text(turnOver1);
   $(".player1-score").text(player1.totalScore);
   $(".player1-roll").text(dice);
@@ -121,15 +123,14 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".btn-roll2").click(function(event){
   event.preventDefault();
-  dice = Rolldice();
+  dice = Player.prototype.Rolldice();
   diceLog2.push(dice);
-  var turnOver2 = turnOver(dice, player2);
-  roundScore = calculateRoundScore(dice, player2);
+  var turnOver2 = Player.prototype.turnOver(dice, player2);
+  roundScore = Player.prototype.calculateRoundScore(dice, player2);
   console.log("Round score " + roundScore + " dice " + dice);
  // score = calculateTotalScore(roundScore, player2);
-  
+ $(".player2-roundScore").text(roundScore);
  $(".playerTurn").text(turnOver2);
- 
  $(".player2-score").text(player2.totalScore);
  $(".player2-roll").text(dice);
   });
@@ -138,29 +139,31 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".btn-hold1").click(function(event){
   event.preventDefault();
-  score = calculateTotalScore(roundScore, player1);
-  roundScore = resetRoundScore(roundScore, player1);
+  score = Player.prototype.calculateTotalScore(player1);
+  //score = Player.prototype.calculateTotalScore(roundScore, player1);
+  roundScore = Player.prototype.resetRoundScore(roundScore, player1);
   console.log("Reset round score " + roundScore + " roundScore for player 1 should be 0: " + player1.score);
-  winner = playerWon(score, player1);
+  winner = Player.prototype.playerWon(score, player1);
   $(".winner").text(winner);
   $(".player1-score").text(score);
   var player1Hold = "Player 1 holds. Player 2's turn";
   $(".playerTurn").text(player1Hold);
-  disableTurn(player1Hold);
+  Player.prototype.disableTurn(player1Hold);
   });
 });
 
 $(document).ready(function() {
   $(".btn-hold2").click(function(event){
   event.preventDefault();
-  score = calculateTotalScore(roundScore, player2);
-  roundScore = resetRoundScore(roundScore, player2);
+  score = Player.prototype.calculateTotalScore(player2);
+  //score = Player.prototype.calculateTotalScore(roundScore, player2);
+  roundScore = Player.prototype.resetRoundScore(roundScore, player2);
   console.log("Reset round score " + roundScore + " roundScore for player 2 should be 0: " + player2.score);
-  winner = playerWon(score, player2);
+  winner = Player.prototype.playerWon(score, player2);
   $(".winner").text(winner);
   $(".player2-score").text(score);
   var player2Hold = "Player 2 holds. Player 1's turn";
   $(".playerTurn").text(player2Hold);
-  disableTurn(player2Hold);
+  Player.prototype.disableTurn(player2Hold);
   });
 });
